e17d57c2e7fd597781c0519458b285fd2a61313a
Elvinas V <elvinas.val@gmail.com>
2022-07-15 16:32:26 +0300

    chore: add and create store controller

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get("/", storeController.homePage);
+
+router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
 
 router.get("/reverse/:name", (req, res) => {
   // res.send("it works");

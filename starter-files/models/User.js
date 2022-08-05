d48241f933d71fe0f8189a79b622f52096509e9c
Elvinas V <elvinas.val@gmail.com>
2022-08-05 18:01:43 +0300

    feat: auth new user

--- starter-files/models/User.js
+++ starter-files/models/User.js
@@ -4,9 +4,9 @@ mongoose.Promise = global.Promise;
 const md5 = require("md5");
 const validator = require("validator");
 const mongodbErrorHandler = require("mongoose-mongodb-errors");
-const passportLocalMongoose = require("password-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userScheme = new Schema({
+const userSchema = new Schema({
   email: {
     type: String,
     unique: true,
@@ -22,7 +22,7 @@ const userScheme = new Schema({
   },
 });
 
-userScheme.plugin(passportLocalMongoose, { usernameField: "email" });
-userScheme.plugin(mongodbErrorHandler);
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model("User", userSchema);
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
 const { catchErrors } = require("../handlers/errorHandlers");
 
@@ -36,6 +37,11 @@ router.get("/register", userController.registerForm);
 // 1. Validate the registration data
 // 2. register the user
 // 3. log the user in
-router.post("/register", userController.validateRegister);
+router.post(
+  "/register",
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 module.exports = router;
